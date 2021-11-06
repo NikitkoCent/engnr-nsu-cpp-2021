780ab9658c86b88612e51e33f5dab47487ccf82f
Danil Taranets <d.taranets@g.nsu.ru>
2021-11-07 01:28:26 +0700

    try6

--- 1/main.cpp
+++ 1/main.cpp
@@ -4,24 +4,18 @@
 #include <cstring>
 
 
-
 int main(int argc, char *argv[]) {
     std::string filepath, mode;
     std::string help = "Usage: ./helper -m <mode> <filename> or ./helper <filename> -m <mode> \nMode types: adler32 or sum64";
 
 
-    if(argc == 4){
+    if (argc == 4) {
         if (strcmp(argv[1], "-m") == 0) {
-            if (strcmp(argv[2], "-m") == 0) {
-                if (strcmp(argv[3], "adler32") == 0 || strcmp(argv[3], "sum64") == 0) {
-                    mode = argv[3];
-                    filepath = argv[2];
-                } else {
-                    std::cerr << help << 1 <<  std::endl;
-                    return 1;
-                }
+            if (strcmp(argv[2], "adler32") == 0 || strcmp(argv[2], "sum64") == 0) {
+                mode = argv[2];
+                filepath = argv[3];
             } else {
-                std::cerr << help << 2 << std::endl;
+                std::cerr << help << 1 << std::endl;
                 return 1;
             }
         } else {
@@ -33,7 +27,7 @@ int main(int argc, char *argv[]) {
                 return 1;
             }
         }
-    }else{
+    } else {
         std::cerr << help << 4 << std::endl;
         return 1;
     }
@@ -41,30 +35,30 @@ int main(int argc, char *argv[]) {
     std::ifstream file;
     file.open(filepath);
 
-    if(!(file.is_open())){
+    if (!(file.is_open())) {
         std::cerr << help << 5 << std::endl;
         return 1;
     }
 
-    try{
-        if(mode == "adler32"){
+    try {
+        if (mode == "adler32") {
             uint32_t a = 1, b = 0;
             char s;
-            while(file.read(&s, sizeof(char))){
+            while (file.read(&s, sizeof(char))) {
                 a = (a + s) % 65521;
                 b = (b + a) % 65521;
             }
-            std::cout << std::hex << ((b << 16)|a) << std::endl;
-        }else{
+            std::cout << std::hex << ((b << 16) | a) << std::endl;
+        } else {
             uint64_t contr_sum = 0;
             uint64_t block;
-            while(file.read((char*)&block, 8)){
+            while (file.read((char *) &block, 8)) {
                 std::cout << block << std::endl;
                 contr_sum += block;
             }
             std::cout << std::hex << contr_sum << std::endl;
         }
-    }catch(std::exception const& e){
+    } catch (std::exception const &e) {
         std::cerr << help << 6 << std::endl;
         return 1;
     }
