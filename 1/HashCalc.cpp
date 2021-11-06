b4c6a425bfbc5ecb478522713b486fc5aaec1463
Gumirov Andrey <gavt45@bk.ru>
2021-11-07 01:41:43 +0700

    Test me plz

--- 1/HashCalc.cpp
+++ 1/HashCalc.cpp
@@ -4,6 +4,13 @@
 
 #include "HashCalc.h"
 #include <cstring>
+#include <iostream>
+
+#ifdef _WIN32
+#include <winsock.h>
+#else
+#include <arpa/inet.h>
+#endif
 
 #define HTONLL(x) ((1==htonl(1)) ? (x) : (((uint64_t)htonl((x) & 0xFFFFFFFFUL)) << 32) | htonl((uint32_t)((x) >> 32)))
 #define NTOHLL(x) ((1==ntohl(1)) ? (x) : (((uint64_t)ntohl((x) & 0xFFFFFFFFUL)) << 32) | ntohl((uint32_t)((x) >> 32)))
@@ -14,6 +21,10 @@ uint32_t HashCalc::adler32(std::ifstream &file) {
 
     while (!file.eof()) {
         file.read(&tmp, sizeof(char));
+        std::streamsize bytes = file.gcount();
+        std::cout << "read " << bytes << std::endl;
+        if (!bytes) break;
+
         a = (a + tmp) % MOD_ADLER;
         b = (b + a) % MOD_ADLER;
     }
@@ -22,18 +33,20 @@ uint32_t HashCalc::adler32(std::ifstream &file) {
 }
 
 uint64_t HashCalc::sum64(std::ifstream &file) {
-    uint64_t *tmp = (uint64_t *) calloc(1, sizeof(uint64_t)), sum = 0;
+    uint64_t tmp = 0, sum = 0;
 
     while (!file.eof()) {
-        file.read((char*)tmp, sizeof(uint64_t));
+        file.read((char*)&tmp, sizeof(uint64_t));
         std::streamsize bytes = file.gcount();
-        tmp[0] = NTOHLL(tmp[0]);
-        tmp[0] >>= 8 * (8 - abs(bytes));
+        if (!bytes) break;
+
+        tmp = NTOHLL(tmp);
+        tmp >>= 8 * (8 - abs(bytes));
+
 //        printf("read: %llx\n", tmp[0]);
 //        tmp[0] = NTOHLL(tmp[0]); // Ensure big-endian
 //        printf("after: %llx\n", tmp[0]);
-        sum += tmp[0];
-        memset(tmp, 0, sizeof(uint64_t));
+        sum += tmp;
     }
     return sum;
 }
