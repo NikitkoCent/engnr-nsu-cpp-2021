f6183d4294f9923de75efb77ab6b56ebaff520f4
Danil Taranets <d.taranets@g.nsu.ru>
2021-11-10 21:33:10 +0700

    Change sum64 again x4

--- 1/main.cpp
+++ 1/main.cpp
@@ -57,7 +57,7 @@ int main(int argc, char *argv[]) {
                 uint64_t block;
                 file.read((char *) &block, sizeof(uint64_t));
 //                block = NTOHLLL(block);
-                block = (((uint64_t)ntohl((block) & 0xFFFFFFFFUL)) << 32) | ntohl((uint32_t)((block) >> 32)));
+                block = (((uint64_t)ntohl((block) & 0xFFFFFFFFUL)) << 32) | ntohl((uint32_t)((block) >> 32));
                 if (file.gcount() != 8){
 //                    block <<= 8*(8-file.gcount());
                     block >>= 8*(8-file.gcount());
