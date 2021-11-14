04ec1f27753afc749257f19b24d44103a9d42e4c
Danil Taranets <d.taranets@g.nsu.ru>
2021-11-14 14:22:50 +0700

    Fixes x4

--- 1/hashFunctions.cpp
+++ 1/hashFunctions.cpp
@@ -26,17 +26,27 @@ void sum64(std::ifstream &file){
     while (!file.eof()) {
         uint64_t block = 0;
         file.read((char *) &block, sizeof(uint64_t));
-//        std::bitset<64> x(block);
+//        std::bitset<128> x(block);
 //        std::cout << x << " " << file.gcount() <<  std::endl;
-//        std::bitset<64> y(block);
-//        std::cout << y << " " << file.gcount() << std::endl;
-        if (file.gcount() == 8){
-            block = (((uint64_t)ntohl((block) & 0xFFFFFFFFUL)) << 32) | ntohl((uint32_t)((block) >> 32));
-
+        uint64_t res = 0;
+        for(int i = 7; i>=0; i--){
+            uint64_t blockb = block;
+            blockb >>= (7-i)*8;
+            blockb <<= 56;
+            blockb >>= (7-i)*8;
+            res += blockb;
         }
-//        std::bitset<64> z(block);
+//        block = (((uint64_t)ntohl((block) & 0xFFFFFFFFUL)) << 32) | ntohl((uint32_t)((block) >> 32));
+//        std::bitset<128> r(block);
+//        std::cout << r << " " << file.gcount() << std::endl;
+//        std::bitset<128> y(res);
+//        std::cout << y << " res " << file.gcount() << std::endl;
+        if (file.gcount() != 8){
+            res >>= 64-8*file.gcount();
+        }
+//        std::bitset<128> z(block);
 //        std::cout << z << " " << file.gcount() << std::endl;
-        contr_sum += block;
+        contr_sum += res;
     }
     std::cout << std::hex << contr_sum << std::endl;
 }
