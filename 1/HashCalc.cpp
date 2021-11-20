9a44104daa3b5ffc6dc632b800aec1daf64272af
Gumirov Andrey <gavt45@bk.ru>
2021-11-20 21:57:32 +0700

    Prettify code a bit

--- 1/HashCalc.cpp
+++ 1/HashCalc.cpp
@@ -6,14 +6,13 @@
 #include <cstring>
 #include <iostream>
 
-uint32_t HashCalc::adler32(std::ifstream &file) {
+uint32_t HashCalc::adler32(std::istream &file) {
     unsigned char tmp;
     uint32_t a = 1, b = 0;
 
     while (!file.eof()) {
         file.read((char *)&tmp, sizeof(unsigned char));
         std::streamsize bytes = file.gcount();
-//        std::cout << "read " << bytes << std::endl;
         if (!bytes) break;
 
         a = (a + tmp) % MOD_ADLER;
@@ -23,7 +22,7 @@ uint32_t HashCalc::adler32(std::ifstream &file) {
     return (b << 16) | a;
 }
 
-uint64_t HashCalc::read_uint64(std::ifstream &file) {
+uint64_t HashCalc::read_uint64(std::istream &file) {
     unsigned char tmp;
     uint64_t num = 0, c = 0;
 
@@ -31,33 +30,18 @@ uint64_t HashCalc::read_uint64(std::ifstream &file) {
         file.read((char*)&tmp, sizeof(unsigned char));
         std::streamsize bytes = file.gcount();
         if (!bytes) break;
-//        printf("Read one byte: %x\n", tmp);
 
         num <<= 8; num |= tmp;
-
         c++;
     }
-//    printf("Read one num: %llx\n", num);
     return num;
 }
 
-uint64_t HashCalc::sum64(std::ifstream &file) {
-    uint64_t sum = 0, tmp;
+uint64_t HashCalc::sum64(std::istream &file) {
+    uint64_t sum = 0;
 
-    while (!file.eof()) {
-//        file.read((char*)&tmp, sizeof(uint64_t));
-//        std::streamsize bytes = file.gcount();
-//        if (!bytes) break;
-//        tmp = read_uint64(file);
-//        tmp = NTOHLL(tmp);
-//        tmp >>= 8 * (8 - abs(bytes));
-        tmp = read_uint64(file);
-//        printf("read: %llx\n", tmp);
-//        printf("read: %llx\n", tmp[0]);
-//        tmp[0] = NTOHLL(tmp[0]); // Ensure big-endian
-//        printf("after: %llx\n", tmp[0]);
+    while (!file.eof())
+        sum += read_uint64(file);
 
-        sum += tmp;
-    }
     return sum;
 }
--- 1/HashCalc.h
+++ 1/HashCalc.h
@@ -11,11 +11,11 @@
 
 class HashCalc {
 public:
-    static uint32_t adler32(std::ifstream &file);
-    static uint64_t sum64(std::ifstream &file);
+    static uint32_t adler32(std::istream &file);
+    static uint64_t sum64(std::istream &file);
 
 private:
-    static uint64_t read_uint64(std::ifstream &file);
+    static uint64_t read_uint64(std::istream &file);
 };
 
 
