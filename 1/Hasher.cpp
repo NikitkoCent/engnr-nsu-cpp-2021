30b126205ce46d31c2b1ce595d6ea4cd19c3669f
Krazerleo <krazerleo@gmail.com>
2021-11-20 23:02:20 +0700

    last fix i hope

--- 1/Hasher.cpp
+++ 1/Hasher.cpp
@@ -4,34 +4,39 @@
 #include <fstream>
 #include <filesystem>
 #include <vector>
-#include <functional>
+#include <cstring>
 
-uint32_t adler32( unsigned char *buf, ptrdiff_t byte_length)
+uint32_t adler32(std::ifstream &ifs)
 {
     uint32_t s1 = 1;
     uint32_t s2 = 0;
-
-    while (byte_length--)
+    unsigned char curr;
+    while (ifs.read(reinterpret_cast<char*>(&curr), sizeof(unsigned char)))
     {
-        s1 = (s1 + *(buf++)) % 65521;
+        s1 = (s1 + curr) % 65521;
         s2 = (s2 + s1) % 65521;
     }
-    return (s2 << 16) + s1;
+    return ((s2 << 16) + s1);
 }
 
-uint64_t sum64( unsigned char *buf, ptrdiff_t byte_length)
+uint64_t sum64(std::ifstream &ifs)
 {
-    uint64_t result = 0;
-    for (int i = 0; i < byte_length; i = i + 8)
-        result += static_cast<uint64_t>(buf[i]) << 56 |
-                  static_cast<uint64_t>(buf[i + 1]) << 48 |
-                  static_cast<uint64_t>(buf[i + 2]) << 40 |
-                  static_cast<uint64_t>(buf[i + 3]) << 32 |
-                  static_cast<uint64_t>(buf[i + 4]) << 24 |
-                  static_cast<uint64_t>(buf[i + 5]) << 16 |
-                  static_cast<uint64_t>(buf[i + 6]) << 8 |
-                  static_cast<uint64_t>(buf[i + 7]) << 0;
-    return result;
+    unsigned char curr;
+    uint64_t result = 0, block = 0;
+    uint32_t cnt = 0;
+    while (ifs.read(reinterpret_cast<char*>(&curr), sizeof(unsigned char)))
+    {
+        block <<= 8;
+        block |= curr;
+        cnt++;
+        if (cnt == 8)
+        {
+            result += block;
+            cnt = 0;
+            block = 0;
+        }
+    }
+    return result + block;
 }
 
 Status hashProccessing(const char *file_path, HashMode hm)
@@ -40,32 +45,21 @@ Status hashProccessing(const char *file_path, HashMode hm)
 
     fs::path abs_path = fs::current_path() / fs::path(file_path);
     std::ifstream ifs(abs_path, std::ifstream::binary);
+
     if (ifs)
     {
         std::uint64_t resultSum = 0;
         std::uint32_t resultAdler = 0;
 
-        std::vector<char> buff(1024,0);
-
         if (hm == HashMode::_Adler32)
-        { 
-            while (!ifs.eof())
-            {
-                ifs.read(buff.data(), static_cast<int64_t>(buff.size()));
-                std::streamsize ss = ifs.gcount();
-                resultAdler += adler32(reinterpret_cast<unsigned char*>(buff.data()), ss);
-            }
-            std::cout  << std::hex << resultAdler << std::endl;
+        {
+            resultAdler = adler32(ifs);
+            std::cout << std::hex << resultAdler << std::endl;
             return Status::_SUCCESS;
         }
         if (hm == HashMode::_Hash64)
         {
-            while (!ifs.eof())
-            {
-                ifs.read(buff.data(), static_cast<int64_t>(buff.size()));
-                std::streamsize ss = ifs.gcount();
-                resultSum += sum64(reinterpret_cast<unsigned char*>(buff.data()), ss);
-            }
+            resultSum = sum64(ifs);
             std::cout << std::hex << resultSum << std::endl;
             return Status::_SUCCESS;
         }
--- 1/inc/Hasher.h
+++ 1/inc/Hasher.h
@@ -5,8 +5,8 @@
 
 const uint16_t BUFFER_SIZE = 1024;
 
-uint32_t adler32( unsigned char*, ptrdiff_t);
-uint64_t sum64( unsigned char*, ptrdiff_t);
+uint32_t adler32( std::ifstream&);
+uint64_t sum64( std::ifstream&);
 Status hashProccessing(const char*, HashMode );
 
 
