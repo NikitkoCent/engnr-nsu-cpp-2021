50a2668ba3aeee9b2eb9d1248686d8ffdbd0822e
Krazerleo <70677812+Krazerleo@users.noreply.github.com>
2021-11-20 12:38:07 +0700

    Update install-dependencies.bat

--- .github/buildsteps/windows/install-dependencies.bat
+++ .github/buildsteps/windows/install-dependencies.bat
@@ -1,5 +1,5 @@
 cd "%LAB_INDEX%"    || exit /b 1
 
 vcpkg --version     || exit /b 1
-vcpkg install boost --triplet x64-windows
-vcpkg install boost --triplet x32-windows
+vcpkg install boost-program-options --triplet x64-windows
+vcpkg install boost-program-options --triplet x32-windows
