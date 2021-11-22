fdad143eb02b7a424a93fe3b9155b3fe50fcd632
Krazerleo <70677812+Krazerleo@users.noreply.github.com>
2021-11-22 12:05:22 +0700

    Update main.cpp

--- 3/main.cpp
+++ 3/main.cpp
@@ -6,18 +6,5 @@ using namespace ns_LLIST;
 
 int main()
 {
-    LinkedList<int> li;
-    li.push_back(5);
-    li.push_back(5);
-    li.push_back(6);
-    li.push_back(6);
     
-    LinkedList<int> ll = li;
-    li.print();
-    std::cout << "count = " << li.size() << std::endl;
-    li.reverse();
-    li.print();
-
-    ll.unique();
-    ll.print();
 }
