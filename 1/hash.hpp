7d5985b05ba919d0019119d65bc0096584fa3f62
vlad00k <veselov.vladiken@mail.ru>
2021-12-08 17:21:30 +0700

    change 2

--- 1/hash.hpp
+++ 1/hash.hpp
@@ -47,31 +47,5 @@ public:
 
 
 
-<<<<<<< HEAD
-=======
-int callAdler32(string fileName) {
-    ifstream myfile(fileName);
 
-    if (!myfile) {
-        std::cerr << "File failed to open";
-        return FILE_ERROR;
-    }
 
-    uint32_t result_adler = adler32(fileName);
-    std::cout << std::hex<< result_adler;
-    return 0;
-}
-int callSumm64(string fileName) {
-    ifstream myfile(fileName, std::ios::binary);
-
-    if (!myfile) {
-        std::cerr << "File failed to open";
-        return FILE_ERROR;
-    }
-
-    uint64_t result_sum64 = summ64(fileName);
-    std::cout << std::hex<< result_sum64;
-    return 0;
-}
-
->>>>>>> 8a482a7b84a359cba63f9c858ac91a24862ef370
