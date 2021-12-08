904a1b3ea03df5701033cc5112f3cd8eceaa08c8
vlad00k <veselov.vladiken@mail.ru>
2021-12-08 17:14:44 +0700

    change 1

--- 1/main.cpp
+++ 1/main.cpp
@@ -1,21 +1,30 @@
-#include "hash.hpp"
+#include "hash.cpp"
 int main(int argc, char* argv[])
 {
-    setlocale(LC_ALL, "Russian");
-    ArgHelper* arg = getArg(argc, argv);
-    if (arg == NULL) {
+    ArgHelper arg = getArg(argc, argv);
+    if (arg.error) {
         show_err();
         return -1;
     }
-    if (arg->isHelp()) {
+    if (arg.isHelp()) {
         show_help();
         return 0;
     }
-    if (arg->isMode()) {
-        if (arg->algo == Algo::ADLER32) {
-            callAdler32(arg->path); cout << endl;
-        }else if (arg->algo == Algo::SUM64) {
-            callSumm64(arg->path);
+    if (arg.isMode()) {
+        uint64_t result;
+        std::fstream fs(arg.path, std::ios::in | std::ios::binary);
+
+        if (arg.algo == Algo::ADLER32) {
+            if ((call(fs, adler32, result)) == 0)
+                std::cout << std::hex<< result;
+            else
+                return ALGO_ERROR;
+
+        }else if (arg.algo == Algo::SUM64) {
+            if(! (call(fs, summ64, result)))
+                std::cout << std::hex<< result;
+            else
+                return ALGO_ERROR;
         }
         else
             return ALGO_ERROR;
