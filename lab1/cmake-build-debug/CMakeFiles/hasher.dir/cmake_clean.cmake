cf38af4359acf3348d5837be5a978d6233353aca
Artyom Shmakov <a.shmakov2@g.nsu.ru>
2021-12-12 17:15:03 +0700

    add lab1

---
+++ lab1/cmake-build-debug/CMakeFiles/hasher.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hasher.dir/Hash.cpp.o"
+  "CMakeFiles/hasher.dir/Hash.cpp.o.d"
+  "CMakeFiles/hasher.dir/main.cpp.o"
+  "CMakeFiles/hasher.dir/main.cpp.o.d"
+  "hasher"
+  "hasher.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hasher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
