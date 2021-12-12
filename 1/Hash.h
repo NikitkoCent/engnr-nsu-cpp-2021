4e92f90956e2513697fe464cecb25f3a9d200006
Artyom Shmakov <a.shmakov2@g.nsu.ru>
2021-12-12 19:11:21 +0700

    add lab1

---
+++ 1/Hash.h
@@ -0,0 +1,15 @@
+//
+// Created by Артем Шмаков on 11.12.2021.
+//
+
+#ifndef LAB1_HASH_H
+#define LAB1_HASH_H
+
+#include <iostream>
+
+uint32_t adler32(std::istream &file);
+uint64_t sum64(std::istream &file);
+
+
+#endif //LAB1_HASH_H
+
