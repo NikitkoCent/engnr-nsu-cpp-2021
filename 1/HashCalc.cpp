74d0f8b1976d2f5156b1937e9a6361bd8de538a5
d.shestakova <d.shestakova2@g.nsu.ru>
2021-12-13 23:55:37 +0700

    Fix Adler32

--- 1/HashCalc.cpp
+++ 1/HashCalc.cpp
@@ -7,8 +7,8 @@
 uint32_t HashCalc::Adler32(std::ifstream &file) {
     uint32_t s1 = 1;
     uint32_t s2 = 0;
-    char cur_byte;
-    while (file.read(&cur_byte, sizeof(cur_byte)))
+    unsigned char cur_byte;
+    while (file.read((char *)&cur_byte, sizeof(cur_byte)))
     {
         s1 = (s1 + cur_byte) % 65521;
         s2 = (s1 + s2) % 65521;
