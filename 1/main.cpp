f36c1b7c7f5020f13e8f2b74c9eec2a18b1dbdba
d.shestakova <d.shestakova2@g.nsu.ru>
2021-12-13 23:24:26 +0700

    Fix ParseArgs

--- 1/main.cpp
+++ 1/main.cpp
@@ -29,7 +29,10 @@ int main(int ac, char **av) {
             filename = std::string(av[i]);
         }
     }
-
+    if (help_mode && mode.empty() && filename.empty()) {
+        std::cout << "usage: [-h | --help] [-m <adler32; sum64> | --mode <adler32; sum64>] <filename>" ;
+        return 0;
+    }
     if (mode != "adler32" && mode != "sum64") {
         // std::cerr << mode << std::endl;
         std::cerr << "Mode must be adler32 or sum64" << std::endl;
@@ -51,10 +54,7 @@ int main(int ac, char **av) {
         return 1;
     }
 
-    if (help_mode) {
-        std::cout << "usage: [-h | --help] [-m <adler32; sum64> | --mode <adler32; sum64>] <filename>" ;
-        return 0;
-    }
+
     // endregion
 
 
