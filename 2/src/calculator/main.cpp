02c6d4ceb17a760262d24edc549736e1aedfa292
Danil Taranets <d.taranets@g.nsu.ru>
2021-12-16 01:34:02 +0700

    Windows error1

--- 2/src/calculator/main.cpp
+++ 2/src/calculator/main.cpp
@@ -7,27 +7,27 @@
 
 
 int main(int argc, char *argv[]) {
-    std::string filepath;
-    std::string help = "Usage: ./Calculator <filepath> or ./Calculator\n"
-                       "Commands: #, POP, PUSH <number>, PUSH <varname>, PEEK <varname>, ABS, PLUS, MINUS, MUL, DIV, PRINT, READ.";
-    bool flag = false;
-
-    if(argc > 1){
-        if(strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "-help") == 0){
-            std::cout << help << std::endl;
-            return 0;
-        }
-        if(argc == 2){
-            filepath = argv[1];
-        }else{
-            std::cerr << "Wrong usage." << help << std::endl;
-            return 1;
-        }
-    }else{
-        flag = true;
-    }
-
     try {
+        std::string filepath;
+        std::string help = "Usage: ./Calculator <filepath> or ./Calculator\n"
+                           "Commands: #, POP, PUSH <number>, PUSH <varname>, PEEK <varname>, ABS, PLUS, MINUS, MUL, DIV, PRINT, READ.";
+        bool flag = false;
+
+        if (argc > 1) {
+            if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "-help") == 0) {
+                std::cout << help << std::endl;
+                return 0;
+            }
+            if (argc == 2) {
+                filepath = argv[1];
+            } else {
+                std::cerr << "Wrong usage." << help << std::endl;
+                return 1;
+            }
+        } else {
+            flag = true;
+        }
+
         if (flag) {
             OneCommandRead();
         } else {
@@ -41,7 +41,7 @@ int main(int argc, char *argv[]) {
             }
             ReadFromStream(file);
         }
-    }catch(std::exception &e){
+    } catch (std::exception &e) {
         std::cerr << e.what() << std::endl;
         return 1;
     }
