03f7864d573473309623b448e8d5d763819e3701
DedMoroz132 <dedmoroz123132@gmail.com>
2021-12-17 00:36:27 +0700

    Fix stupid coding faults

--- 1/HashFunc.h
+++ 1/HashFunc.h
@@ -1,7 +1,6 @@
 ﻿#pragma once
 #include <iostream>
 #include <fstream>
-#include <iostream>
 
-extern uint32_t adler32(std::istream& file);
-extern uint64_t sum64(std::istream& file);
+uint32_t adler32(std::istream& file);
+uint64_t sum64(std::istream& file);
