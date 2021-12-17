79a3cae47867d4006e942dd2ed3b3dafff3d2454
artemlin <44076910+artemlin@users.noreply.github.com>
2021-12-17 22:07:33 +0700

    Update Functions.cpp

--- 1/Functions.cpp
+++ 1/Functions.cpp
@@ -15,18 +15,26 @@ uint16_t adler32(std::ifstream& file)
 }
 
 
-uint16_t sum64(std::ifstream& file) {
+uint64_t sum64(std::ifstream& file) {
     uint64_t result = 0;
     uint64_t part = 0;
-
-    while (file.read((char*)&part, sizeof(uint64_t)))
+    uint64_t element = 0;
+    uint64_t counter = 0;
+    while (file.read((char*)&element, sizeof(char)))
     {
-        part *= 256;
-        ~part;
-        part <<= 8;
-        part *= 256;
-        result += part;
+        part += element;
+        counter++;
+        if (counter == 8) {
+            result += part;
+            counter = 0;
+        }
+        part << 8;
+        
 
+    }
+    if (counter != 0) {
+        part >> 8;
+        result += part;
     }
     return result;
 }
