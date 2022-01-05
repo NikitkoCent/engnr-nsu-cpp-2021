561c4ba49cc6467cbb2ead63b0b613cbff3cfa1a
ArtyomSh <71187863+ArtyomSh@users.noreply.github.com>
2022-01-05 14:58:30 +0700

    Delete 2/cmake-build-debug directory

--- 2/cmake-build-debug/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/artemsmakov/engnr-nsu-cpp-2021/2/tests
-# Build directory: /Users/artemsmakov/engnr-nsu-cpp-2021/2/cmake-build-debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[googletest]=] "/Users/artemsmakov/engnr-nsu-cpp-2021/2/cmake-build-debug/tests/calculator_tests")
-set_tests_properties([=[googletest]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/artemsmakov/engnr-nsu-cpp-2021/2/tests/CMakeLists.txt;16;add_test;/Users/artemsmakov/engnr-nsu-cpp-2021/2/tests/CMakeLists.txt;0;")
