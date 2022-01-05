248a2d0d29075cd3dbb961d8ce3a3ef0fd9a6765
ArtyomSh <71187863+ArtyomSh@users.noreply.github.com>
2022-01-05 14:59:05 +0700

    Delete 2/sborka directory

--- 2/sborka/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/artemsmakov/engnr-nsu-cpp-2021/2/googletest/googlemock
-# Build directory: /Users/artemsmakov/engnr-nsu-cpp-2021/2/sborka/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
