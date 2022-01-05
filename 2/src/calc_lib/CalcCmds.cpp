0e0b7eb51788765d685e16b110249b0bc9fb6af7
Krazerleo <krazerleo@gmail.com>
2022-01-05 13:28:24 +0700

    fixed abs

--- 2/src/calc_lib/CalcCmds.cpp
+++ 2/src/calc_lib/CalcCmds.cpp
@@ -43,14 +43,13 @@ void abs_cmd::execute(CalcContext &_calc)
     {
         auto item = _calc.m_stack.top();
         _calc.m_stack.pop();
-        if (item.Ref() == -9223372036854775808)
-            throw std::runtime_error("Invalid absolute value");
-        if (item.Ref() >= 0)
-            _calc.m_stack.push(item);
-        else
+        try
         {
-            int64_t temp = *item.data_ptr();
-            _calc.m_stack.push(SafeInt<int64_t>(-temp));
+            item > 0 ? _calc.m_stack.push(item) : _calc.m_stack.push(-item);
+        }
+        catch (const SafeIntException& e)
+        {
+            throw std::runtime_error("Invalid absolute value");
         }
     }
 }
@@ -205,9 +204,12 @@ std::unique_ptr<abstract_command> CreateAbstCmd(std::string &command)
 
 inline std::string trim(const std::string &s)
 {
-   auto wsfront=std::find_if_not(s.begin(),s.end(),[](int c){return std::isspace(c);});
-   auto wsback=std::find_if_not(s.rbegin(),s.rend(),[](int c){return std::isspace(c);}).base();
-   return (wsback<=wsfront ? std::string() : std::string(wsfront,wsback));
+    auto wsfront = std::find_if_not(s.begin(), s.end(), [](int c)
+                                    { return std::isspace(c); });
+    auto wsback = std::find_if_not(s.rbegin(), s.rend(), [](int c)
+                                   { return std::isspace(c); })
+                      .base();
+    return (wsback <= wsfront ? std::string() : std::string(wsfront, wsback));
 }
 
 std::vector<std::string> split(const std::string &s)
