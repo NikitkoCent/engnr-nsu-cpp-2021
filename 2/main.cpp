78c633a999bd221043aa806da7ab93d9d00ad193
d.shestakova <d.shestakova2@g.nsu.ru>
2022-01-07 19:37:39 +0700

    Fix shared_ptr on Ubuntu

--- 2/main.cpp
+++ 2/main.cpp
@@ -4,35 +4,35 @@
 
 using namespace std;
 
-unique_ptr<Command> get_command_by_string(string &cur_string, Context &context) {
-    unique_ptr<Command> operation;
+shared_ptr<Command> get_command_by_string(string &cur_string, Context &context) {
+    shared_ptr<Command> operation;
     if (cur_string.empty()) {
         operation = nullptr;
     } else if (cur_string == "POP") {
-        operation = unique_ptr<Command>(new Pop(context));
+        operation = shared_ptr<Command>(new Pop(context));
     } else if (cur_string == "PUSH") {
-        operation = unique_ptr<Command>(new Push(context));
+        operation = shared_ptr<Command>(new Push(context));
     } else if (cur_string == "PEEK") {
-        operation = unique_ptr<Command>(new Peek(context));
+        operation = shared_ptr<Command>(new Peek(context));
     } else if (cur_string == "ABS") {
-        operation = unique_ptr<Command>(new Abs(context));
+        operation = shared_ptr<Command>(new Abs(context));
     } else if (cur_string == "PLUS") {
-        operation = unique_ptr<Command>(new Plus(context));
+        operation = shared_ptr<Command>(new Plus(context));
     } else if (cur_string == "MINUS") {
-        operation = unique_ptr<Command>(new Minus(context));
+        operation = shared_ptr<Command>(new Minus(context));
     } else if (cur_string == "MUL") {
-        operation = unique_ptr<Command>(new Mul(context));
+        operation = shared_ptr<Command>(new Mul(context));
     } else if (cur_string == "DIV") {
-        operation = unique_ptr<Command>(new Div(context));
+        operation = shared_ptr<Command>(new Div(context));
     } else if (cur_string == "PRINT") {
-        operation = unique_ptr<Command>(new Print(context));
+        operation = shared_ptr<Command>(new Print(context));
     } else if (cur_string == "READ") {
-        operation = unique_ptr<Command>(new Read(context));
+        operation = shared_ptr<Command>(new Read(context));
     } else {
         if (cur_string != "#") {
             throw runtime_error("Unknown command: " + cur_string);
         } else {
-            operation = unique_ptr<Command>(new Comment(context));
+            operation = shared_ptr<Command>(new Comment(context));
         }
     }
     return operation;
