50933b36fb9eb9b1bc4b7db46a49c9de26d87847
artemlin <8189560-artemlin@users.noreply.gitlab.com>
2022-01-07 13:12:13 +0700

    test

--- 2/tests/test.cpp
+++ 2/tests/test.cpp
@@ -102,47 +102,8 @@ TEST(TestCalc, Test_divByZero) {
 }
 
 
-TEST(TestCalc, Test_divByZero) {
-	std::stringstream line_cmd("# (10 + 5*36)/0 PRINT"
-		"PUSH 10 \n"
-		"PUSH 5 \n"
-		"PUSH 36 \n"
-		"MUL \n"
-		"SUM \n"
-		"PUSH 0 \n"
-		"DIV \n"
-		"PUSH 15 \n"
-		"SUM \n"
-		"PRINT \n");
-	string filename("tests/test_example/test.txt");
-	Param p;
-	string line;
-	OperationsFactory create_stack;
-	Operations* cmd_stack;
-	ifstream in(filename);
-	while (getline(in, line))
-	{
-		split(p, line, ' ');
-		if (p.vst.size() >= 1)
-		{
 
-			try {
-				if (check(p)) {
-					cmd_stack = create_stack.Create(p);
-					cmd_stack->execute(p);
-				}
-				p.vst.clear();
-			}
-			catch (Exception& expn) {
-				std::cerr << expn.what();
-				EXPECT_EQ(expn.what(), string("Error: Division by 0"));
-
-			}
-		}
-	}
-}
-
-TEST(TestCalc, Test_divByZero) {
+TEST(TestCalc, Test_empty_stack) {
 	std::stringstream line_cmd("# PUSH 5 POP POP"
 		"PUSH 5 \n"
 		"POP \n"
