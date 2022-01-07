0786cb4af55ef0fe0243c1202ec4c9b86bc5dfd3
artemlin <8189560-artemlin@users.noreply.gitlab.com>
2022-01-07 13:04:39 +0700

    test

--- 2/tests/test.cpp
+++ 2/tests/test.cpp
@@ -1,9 +1,9 @@
-#include "D:\Server\gitlab\Git\engnr-nsu-cpp-2021\2\googletest\googletest\include\gtest\gtest.h"
+#include "gtest/gtest.h"
 #include "Lab_2.h" 
 #include "Lab_2.cpp"
 
 TEST(TestCalc, Test1) {
-	std::stringstream input_cmd("# (10 + 5*36)/4 + 15 PRINT"
+	std::stringstream line_cmd("# (10 + 5*36)/4 + 15 PRINT"
 		"PUSH 10 \n"
 		"PUSH 5 \n"
 		"PUSH 36 \n"
@@ -14,15 +14,161 @@ TEST(TestCalc, Test1) {
 		"PUSH 15 \n"
 		"SUM \n"
 		"PRINT \n");
+	Param p;
+	string line;
+	OperationsFactory create_stack;
+	Operations* cmd_stack;
 	int64_t result = 62;
-	testing::internal::CaptureStdout();
-	std::cout << "PUSH 4";
-	std::string output = testing::internal::GetCapturedStdout();
-	EXPECT_EQ(output, result);
+	while (getline(line_cmd, line))
+	{
+		split(p, line, ' ');
+		if (p.vst.size() >= 1)
+		{
+
+		if (check(p)) {
+					cmd_stack = create_stack.Create(p);
+					cmd_stack->execute(p);
+				}
+				p.vst.clear();
+			
+		}
+	}
+	EXPECT_EQ(p.stk.top(), result);
 }
 
 
-int main(int argc, char** argv) {
-	testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+TEST(TestCalc, Test_example) {
+	string filename("tests/test_example/test.txt");
+	Param p;
+	string line;
+	OperationsFactory create_stack;
+	Operations* cmd_stack;
+	int64_t result = 220;
+	ifstream in(filename);
+	while (getline(in, line))
+	{
+		split(p, line, ' ');
+		if (p.vst.size() >= 1)
+		{
+			if (check(p)) {
+				cmd_stack = create_stack.Create(p);
+				cmd_stack->execute(p);
+			}
+			p.vst.clear();
+
+		}
+	}
+	EXPECT_EQ(p.stk.top(), result);
+}
+
+TEST(TestCalc, Test_divByZero) {
+	std::stringstream line_cmd("# (10 + 5*36)/0 PRINT"
+		"PUSH 10 \n"
+		"PUSH 5 \n"
+		"PUSH 36 \n"
+		"MUL \n"
+		"SUM \n"
+		"PUSH 0 \n"
+		"DIV \n"
+		"PUSH 15 \n"
+		"SUM \n"
+		"PRINT \n");
+	string filename("tests/test_example/test.txt");
+	Param p;
+	string line;
+	OperationsFactory create_stack;
+	Operations* cmd_stack;
+	ifstream in(filename);
+	while (getline(cin, line))
+	{
+		split(p, line, ' ');
+		if (p.vst.size() >= 1)
+		{
+
+			try {
+				if (check(p)) {
+					cmd_stack = create_stack.Create(p);
+					cmd_stack->execute(p);
+				}
+				p.vst.clear();
+			}
+			catch (Exception& expn) {
+				std::cerr << expn.what();
+				EXPECT_EQ(expn.what(), string("Error: Division by 0"));
+
+			}
+		}
+	}
+}
+
+
+TEST(TestCalc, Test_divByZero) {
+	std::stringstream line_cmd("# (10 + 5*36)/0 PRINT"
+		"PUSH 10 \n"
+		"PUSH 5 \n"
+		"PUSH 36 \n"
+		"MUL \n"
+		"SUM \n"
+		"PUSH 0 \n"
+		"DIV \n"
+		"PUSH 15 \n"
+		"SUM \n"
+		"PRINT \n");
+	string filename("tests/test_example/test.txt");
+	Param p;
+	string line;
+	OperationsFactory create_stack;
+	Operations* cmd_stack;
+	ifstream in(filename);
+	while (getline(in, line))
+	{
+		split(p, line, ' ');
+		if (p.vst.size() >= 1)
+		{
+
+			try {
+				if (check(p)) {
+					cmd_stack = create_stack.Create(p);
+					cmd_stack->execute(p);
+				}
+				p.vst.clear();
+			}
+			catch (Exception& expn) {
+				std::cerr << expn.what();
+				EXPECT_EQ(expn.what(), string("Error: Division by 0"));
+
+			}
+		}
+	}
+}
+
+TEST(TestCalc, Test_divByZero) {
+	std::stringstream line_cmd("# PUSH 5 POP POP"
+		"PUSH 5 \n"
+		"POP \n"
+		"POP \n");
+	Param p;
+	string line;
+	OperationsFactory create_stack;
+	Operations* cmd_stack;
+	while (getline(line_cmd, line))
+	{
+		split(p, line, ' ');
+		if (p.vst.size() >= 1)
+		{
+
+			try {
+				if (check(p)) {
+					cmd_stack = create_stack.Create(p);
+					cmd_stack->execute(p);
+				}
+				p.vst.clear();
+			}
+			catch (Exception& expn) {
+				std::cerr << expn.what();
+				EXPECT_EQ(expn.what(), string("Error: Empty Stack"));
+
+			}
+		}
+	}
 }
