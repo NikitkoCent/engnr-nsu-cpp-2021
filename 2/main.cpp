de347fbe814603d1c9a97f100f553513464f091e
Fossamar <piskeeva2002@bk.ru>
2022-01-08 16:01:11 +0700

    Hooray!

--- 2/main.cpp
+++ 2/main.cpp
@@ -13,24 +13,6 @@ using std:: ifstream;
 
 int main(int argc, char *argv[]) {
 
-//    std::ifstream in;
-//    StackCalculator stackCalculator;
-//    try {
-//        if (argc > 1) {
-//            in.open(argv[1]);
-//            stackCalculator.parse_stream(in);
-//        } else {
-//            stackCalculator.parse_stream(cin);
-//        }
-//    } catch (std::exception &e) {
-//        std::cerr << e.what() << endl;
-//        return 1;
-//    }
-//    return 0;
-//}
-
-
-
     try {
         string filepath;
 
