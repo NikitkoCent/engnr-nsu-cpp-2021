8449cdd5bd08bb2e7df761c4e687b459c8de8bc1
zMazerlord <v.kolobenko@g.nsu.ru>
2022-01-08 16:58:11 +0700

    test

---
+++ 1/funk_adl_sum.h
@@ -0,0 +1,16 @@
+//
+// Created by zmazerlord on 07.01.2022.
+//
+
+#ifndef INC_1_FUNK_ADL_SUM_H
+#define INC_1_FUNK_ADL_SUM_H
+
+
+#include <iostream>
+#include <fstream>
+#include <cstdint>
+
+uint32_t adler32(std::ifstream&);
+uint64_t sum64( std::ifstream&);
+
+#endif
