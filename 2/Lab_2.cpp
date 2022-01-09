c5bef3c068171ac0ad19f903fc2b6c2998ca2c8f
artemlin <8189560-artemlin@users.noreply.gitlab.com>
2022-01-09 14:52:09 +0700

    lab_cpp_stream

--- 2/Lab_2.cpp
+++ 2/Lab_2.cpp
@@ -2,80 +2,63 @@
 #include "Operations.h"
 
 
+void input_processing(ifstream &ins, string &line, Param &p) {
+    OperationsFactory create_stack;
+    Operations* cmd_stack;
+    split(p, line, ' ');
+    if (p.vst.size() >= 1)
+    {
+        if (check(p)) {
+            cmd_stack = create_stack.Create(p);
+            cmd_stack->execute(p);
+            delete cmd_stack;
+         }
+         p.vst.clear();
+    }
+
+}
 int main(int argc, char* argv[]) {
 
     std::string s;
     Param p;
     string filename;
     string line;
-    OperationsFactory create_stack;
-    Operations *cmd_stack;
     if (argc >= 2)
     {
         filename = argv[1];
     }
     ifstream in(filename);
-    if (in.is_open())
-    {
-
-        while (getline(in, line))
-        {
-
-            split(p, line, ' ');
-            if (p.vst.size() >= 1)
-            {
-                
-                try {
-                    if (check(p)){
-                        cmd_stack = create_stack.Create(p);
-                        cmd_stack->execute(p);
-                        delete cmd_stack;
-                    }
-                    p.vst.clear();
-                }
-                catch (Exception &expn){
-                    std::cerr << expn.what();
-                    return 1;
-                }
-                catch (const char* exceptions) {
-                    std::cerr << exceptions;
-                    return 1;
-                }
-            }
-        }
-    }
-    else if (argc >= 2 && in.is_open() == 1)
+    if (argc >= 2 && in.is_open() == 1)
     {
         std::cerr << "File not found" << endl;
         return 1;
     }
-    else
-    {
-        while (getline(cin, line))
-        {
-            split(p, line, ' ');
-            if (p.vst.size() >= 1)
+    else {
+        try {
+            if (in.is_open())
             {
-
-                try {
-                    if (check(p)){
-                        cmd_stack = create_stack.Create(p);
-                        cmd_stack->execute(p);
-                        delete cmd_stack;
-                    }
-                    p.vst.clear();
-                }
-                catch (Exception &expn) {
-                    std::cerr << expn.what();
-                    return 1;
-                }
-                catch (const char* exceptions) {
-                    std::cerr << exceptions;
-                    return 1;
+                while (getline(in, line)) {
+                    input_processing(ins, line, p);
                 }
             }
+            else {
+                while (getline(cin, line))
+                {
+                    input_processing(ins, line, p);
+                }
+            }
+
+        }
+        catch (Exception& expn) {
+            std::cerr << expn.what();
+            return 1;
+        }
+        catch (const char* exceptions) {
+            std::cerr << exceptions;
+            return 1;
         }
     }
+    
 
     return 0;
 }
